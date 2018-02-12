baad5181714524ee5ce2a0f35a665032f06eba72
cvn_duy_truong <duy.truong@contemi.com.vn>
2018-02-12 18:11:21 +0700

    Init extension project

---
+++ DCReleaseTools/Dialogs/HockeyAuthDialog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DCReleaseTools
+{
+    public partial class HockeyAuthDialog : Gtk.Dialog
+    {
+        public HockeyAuthDialog()
+        {
+            this.Build();
+        }
+    }
+}
