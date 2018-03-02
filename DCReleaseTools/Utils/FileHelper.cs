9f2f72b4070579e71c4e5963ea77fdb67145f687
cvn_duy_truong <duy.truong@contemi.com.vn>
2018-03-02 11:43:29 +0700

    remove duplicate exception

--- DCReleaseTools/Utils/FileHelper.cs
+++ DCReleaseTools/Utils/FileHelper.cs
@@ -119,15 +119,4 @@ namespace DCReleaseTools.Utils
             }
         }
     }
-
-    public class FileRenameToLowercaseException : Exception
-    {
-        public string FileName { get; set; }
-
-        public FileRenameToLowercaseException(string fileName, Exception actualException)
-            : base("Unable to rename " + fileName + " to lowercase", actualException)
-        {
-            FileName = fileName;
-        }
-    }
 }
