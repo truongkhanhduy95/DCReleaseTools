fcf4787e3915d8b342b37746e6bfaf8685dcb64d
cvn_duy_truong <duy.truong@contemi.com.vn>
2018-03-02 11:41:12 +0700

    add application arguments

---
+++ DCReleaseTools/Handlers/DroidBindingControl/AndroidStudioDesign/ApplicationArguments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DCReleaseTools.Handlers
+{
+    public class ApplicationArguments
+    {
+        public string AndroidStudioPath { get; set; }
+
+        public string AndroidSDKPath { get; set; }
+
+        public string XamarinProjectPath { get; set; }
+
+        public string CustomTemplatePath { get; set; }
+    }
+}
